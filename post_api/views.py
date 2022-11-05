d64ad464e1cb38565be4c5e7b94166edc114c056
Praise Temitope Izuagie <izuagiepraise@gmail.com>
2022-11-05 09:10:51 +0100

    Fix JSON parse issue

--- post_api/views.py
+++ post_api/views.py
@@ -1,11 +1,14 @@
 import operator
 from enum import Enum
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,parser_classes
 from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_400_BAD_REQUEST
 from rest_framework.parsers import JSONParser
 
 from .serializers import ArithmeticSerializer
+from .models import ArithmeticQueryModel
 
 
 class OperationEnum(Enum):
@@ -24,12 +27,19 @@ class OperationEnum(Enum):
     times = operator.mul
 
 
+@api_view(["GET"])
+def arithmetic_get_view(request, *args, **kwargs):
+    all_data = ArithmeticQueryModel.objects.all()
+    serializer = ArithmeticSerializer(all_data, many=True)
+    return Response(serializer.data, status=HTTP_200_OK)
+
 @api_view(["POST"])
+@parser_classes([JSONParser])
 def arithmetic_post_view(request, *args, **kwargs):
     """Parse the json content of the request's post and return
     the result of the arithmetic operation in a specified format
     """
-    arithmetic_query = JSONParser().parse(request)
+    arithmetic_query = request.data
     serializer = ArithmeticSerializer(data=arithmetic_query)
     if serializer.is_valid():
         serializer.save()
@@ -48,4 +58,5 @@ def arithmetic_post_view(request, *args, **kwargs):
                 "operation_type": operation_type,
                 "result": result,
             }
-            return Response(response_data)
+            return Response(response_data, status=HTTP_200_OK)
+    return Response(status=HTTP_400_BAD_REQUEST)
