a83ad8cae40d44b5f1f6e1b56d88ce5215fd3cf1
Praise Temitope Izuagie <izuagiepraise@gmail.com>
2022-11-05 09:27:14 +0100

    Fix JSON parse issue

--- post_api/views.py
+++ post_api/views.py
@@ -1,11 +1,10 @@
 import operator
 from enum import Enum
 
-from rest_framework.decorators import api_view,parser_classes
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 from rest_framework.status import HTTP_400_BAD_REQUEST
-from rest_framework.parsers import JSONParser
 
 from .serializers import ArithmeticSerializer
 from .models import ArithmeticQueryModel
@@ -27,14 +26,7 @@ class OperationEnum(Enum):
     times = operator.mul
 
 
-@api_view(["GET"])
-def arithmetic_get_view(request, *args, **kwargs):
-    all_data = ArithmeticQueryModel.objects.all()
-    serializer = ArithmeticSerializer(all_data, many=True)
-    return Response(serializer.data, status=HTTP_200_OK)
-
 @api_view(["POST"])
-@parser_classes([JSONParser])
 def arithmetic_post_view(request, *args, **kwargs):
     """Parse the json content of the request's post and return
     the result of the arithmetic operation in a specified format
