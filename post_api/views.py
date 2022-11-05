8a618fe9636afb389dba982b7997d99012ce5097
Izuagie Praise <41047955+Izugance@users.noreply.github.com>
2022-11-05 16:22:05 +0100

    Testing

--- post_api/views.py
+++ post_api/views.py
@@ -28,29 +28,32 @@ class OperationEnum(Enum):
     times = operator.mul
 
 
-@api_view(["POST"])
+@api_view(["GET", "POST"])
 @permission_classes([AllowAny])
 def arithmetic_post_view(request, *args, **kwargs):
     """Parse the json content of the request's post and return
     the result of the arithmetic operation in a specified format
     """
-    serializer = ArithmeticSerializer(data=request.data)
-    return JsonResponse(request.data)
-    if serializer.is_valid():
-        serializer.save()
-        received_data = serializer.validated_data
-        operation_type = received_data["operation_type"].lower().strip()
-        x = received_data["x"]
-        y = received_data["y"]
-        try:
-            input_operator = OperationEnum[operation_type].value
-        except Exception:
-            return Response({"error": "Unknown operation"})
-        else:
-            result = input_operator(x, y)
-            response_data = {
-                "slackUsername": "@izugance",
-                "operation_type": operation_type,
-                "result": result,
-            }
-            return Response(response_data)
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+            serializer = ArithmeticSerializer(data=request.data)
+            return JsonResponse(request.data)
+            if serializer.is_valid():
+                serializer.save()
+                received_data = serializer.validated_data
+                operation_type = received_data["operation_type"].lower().strip()
+                x = received_data["x"]
+                y = received_data["y"]
+                try:
+                    input_operator = OperationEnum[operation_type].value
+                except Exception:
+                    return Response({"error": "Unknown operation"})
+                else:
+                    result = input_operator(x, y)
+                    response_data = {
+                        "slackUsername": "@izugance",
+                        "operation_type": operation_type,
+                        "result": result,
+                    }
+                    return Response(response_data)
