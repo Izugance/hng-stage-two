89b924b177c848bb1e834f7c2cad480ff9a4ea61
Praise Temitope Izuagie <izuagiepraise@gmail.com>
2022-11-05 12:15:50 +0100

    Fix parse issue

--- post_api/views.py
+++ post_api/views.py
@@ -3,8 +3,6 @@ from enum import Enum
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK
-from rest_framework.status import HTTP_400_BAD_REQUEST
 
 from .serializers import ArithmeticSerializer
 from .models import ArithmeticQueryModel
@@ -31,24 +29,22 @@ def arithmetic_post_view(request, *args, **kwargs):
     """Parse the json content of the request's post and return
     the result of the arithmetic operation in a specified format
     """
-    received_data = request.data
-    # serializer = ArithmeticSerializer(data=arithmetic_query)
-    #if serializer.is_valid():
-    #    serializer.save()
-    #    received_data = serializer.data 
-    operation_type = received_data["operation_type"].lower().strip()
-    x = received_data["x"]
-    y = received_data["y"]
-    try:
-        input_operator = OperationEnum[operation_type].value
-    except Exception:
-        return Response("Unknown operation")
-    else:
-        result = input_operator(x, y)
-        response_data = {
-            "slackUsername": "@izugance",
-            "operation_type": operation_type,
-            "result": result,
-        }
-        return Response(response_data, status=HTTP_200_OK)
-    # return Response(status=HTTP_400_BAD_REQUEST)
+    serializer = ArithmeticSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        received_data = serializer.data
+        operation_type = received_data["operation_type"].lower().strip()
+        x = received_data["x"]
+        y = received_data["y"]
+        try:
+            input_operator = OperationEnum[operation_type].value
+        except Exception:
+            return Response("Unknown operation")
+        else:
+            result = input_operator(x, y)
+            response_data = {
+                "slackUsername": "@izugance",
+                "operation_type": operation_type,
+                "result": result,
+            }
+            return Response(response_data)
