ef79efe130fc12d3731386dc3264c3e927fb47e3
Izuagie Praise <41047955+Izugance@users.noreply.github.com>
2022-11-05 15:45:52 +0100

    Remove request.method check

--- post_api/views.py
+++ post_api/views.py
@@ -33,23 +33,22 @@ def arithmetic_post_view(request, *args, **kwargs):
     """Parse the json content of the request's post and return
     the result of the arithmetic operation in a specified format
     """
-    if request.method == "POST":
-        serializer = ArithmeticSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            received_data = serializer.validated_data
-            operation_type = received_data["operation_type"].lower().strip()
-            x = received_data["x"]
-            y = received_data["y"]
-            try:
-                input_operator = OperationEnum[operation_type].value
-            except Exception:
-                return Response({"error": "Unknown operation"})
-            else:
-                result = input_operator(x, y)
-                response_data = {
-                    "slackUsername": "@izugance",
-                    "operation_type": operation_type,
-                    "result": result,
-                }
-                return Response(response_data)
+    serializer = ArithmeticSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        received_data = serializer.validated_data
+        operation_type = received_data["operation_type"].lower().strip()
+        x = received_data["x"]
+        y = received_data["y"]
+        try:
+            input_operator = OperationEnum[operation_type].value
+        except Exception:
+            return Response({"error": "Unknown operation"})
+        else:
+            result = input_operator(x, y)
+            response_data = {
+                "slackUsername": "@izugance",
+                "operation_type": operation_type,
+                "result": result,
+            }
+            return Response(response_data)
