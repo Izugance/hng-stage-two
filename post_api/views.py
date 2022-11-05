fa53ae02a5c73afa1d45d759c07a150b1bbb7137
Izuagie Praise <41047955+Izugance@users.noreply.github.com>
2022-11-05 18:36:22 +0100

    Fix requests issue

--- post_api/views.py
+++ post_api/views.py
@@ -1,15 +1,12 @@
 import operator
 from enum import Enum
 
-from django.http import JsonResponse
 from rest_framework.decorators import api_view
-from rest_framework.decorators import parser_classes
-from rest_framework.decorators import permission_classes
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED
+from rest_framework.status import HTTP_400_BAD_REQUEST
 
 from .serializers import ArithmeticSerializer
-from .models import ArithmeticQueryModel
 
 
 class OperationEnum(Enum):
@@ -28,32 +25,29 @@ class OperationEnum(Enum):
     times = operator.mul
 
 
-@api_view(["GET", "POST"])
-@permission_classes([AllowAny])
+@api_view(["POST"])
 def arithmetic_post_view(request, *args, **kwargs):
     """Parse the json content of the request's post and return
     the result of the arithmetic operation in a specified format
     """
-    if request.method == "GET":
-        pass
-    elif request.method == "POST":
-            serializer = ArithmeticSerializer(data=request.data)
-            return JsonResponse(request.data)
-            if serializer.is_valid():
-                serializer.save()
-                received_data = serializer.validated_data
-                operation_type = received_data["operation_type"].lower().strip()
-                x = received_data["x"]
-                y = received_data["y"]
-                try:
-                    input_operator = OperationEnum[operation_type].value
-                except Exception:
-                    return Response({"error": "Unknown operation"})
-                else:
-                    result = input_operator(x, y)
-                    response_data = {
-                        "slackUsername": "@izugance",
-                        "operation_type": operation_type,
-                        "result": result,
-                    }
-                    return Response(response_data)
+    if request.method == "POST":
+        serializer = ArithmeticSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            received_data = serializer.data
+            operation_type = received_data["operation_type"].lower().strip()
+            x = received_data["x"]
+            y = received_data["y"]
+            try:
+                input_operator = OperationEnum[operation_type].value
+            except Exception:
+                return Response("Unknown operation")
+            else:
+                result = input_operator(x, y)
+                response_data = {
+                    "slackUsername": "@izugance",
+                    "operation_type": operation_type,
+                    "result": result,
+                }
+                return Response(response_data, status=HTTP_201_CREATED)
+        return Response(status=HTTP_400_BAD_REQUEST)
